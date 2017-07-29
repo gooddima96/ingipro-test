ae418c19f7259d92564fe86c67b84c719a7dcbb9
gooddima96 <gooddima96@yandex.ru>
2017-07-29 12:28:44 +0300

    Added 3613

---
+++ 3613.js
@@ -0,0 +1,39 @@
+/*
+Мы описали пользователя mike и всех его друзей в формате JSON:
+    var mike = {
+        friends: [{
+            name: 'Anna',
+            avatar: {
+                url: 'http://some/url/to/avatar'
+            }
+        }]
+    }
+Вам необходимо получить аватар первого друга mike .
+Если друзей нет или они не загрузили своё фото, то нужно вернуть 'http://default/url/to/avatar'
+*/
+
+'use strict';
+
+var mike = {
+    friends: [{
+        name: 'Anna',
+        avatar: {
+            url: 'http://some/url/to/avatar',
+        },
+    }],
+};
+
+function getProp(obj, path, def) {
+    let result = obj;
+    const keyArr = path.split('.');
+    for (let key in keyArr) {
+        result = result[keyArr[key]];
+        if (result === undefined) {
+            result = def;
+            break;
+        }
+    }
+    return result;
+}
+
+console.log(getProp(mike, 'friends.0.avatar.url', 'http://default/url/to/avatar'));
