7caa2f0a1fcb3be6c1e1404741d090e38421a531
gooddima96 <gooddima96@yandex.ru>
2017-07-30 22:23:54 +0300

    Added 4

---
+++ 4.js
@@ -0,0 +1,19 @@
+/*
+4
+Написать функцию, которая возвращает сумму указанных чисел и всех чисел между ними. Шаг равен 1.
+Первое число необязательно наименьшее
+*/
+
+function sumAll(arr) {
+    let result = 0;
+    let sortCopy = arr.slice().sort((a, b) => a - b);
+
+    for (let i = sortCopy[0]; i <= sortCopy[sortCopy.length - 1]; i++) {
+        result += i;
+    }
+
+    return result;
+}
+
+console.log(sumAll([1, 4])); // 10 (1+2+3+4)
+console.log(sumAll([10, 5])); // 45 (10+9+8+7+6+5)
