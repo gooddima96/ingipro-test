ecc9b710039dd70f59a726d0eb30b9600abb724c
gooddima96 <gooddima96@yandex.ru>
2017-07-30 23:49:25 +0300

    Added 6

---
+++ 6.js
@@ -0,0 +1,15 @@
+/*
+6
+Написать функцию, которая вернет новый объект, у которого ключи стали значениями, а значения ключами
+*/
+
+function invert_key_value(obj) {
+    let result = {};
+    for (let key in obj) {
+        result[obj[key]] = key;
+    }
+    return result;
+}
+
+console.log(invert_key_value({red: '#FF0000', green: '#00FF00', white: '#FFFFFF'}));
+// {#00FF00: "green", #FF0000: "red", #FFFFFF: "white"}
