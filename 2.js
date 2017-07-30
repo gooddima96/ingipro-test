3bff0879b9cc20c91af9489ef10f38394cad9ee9
gooddima96 <gooddima96@yandex.ru>
2017-07-30 19:13:26 +0300

    Added 2

---
+++ 2.js
@@ -0,0 +1,13 @@
+/*
+2
+Написать функцию palindrome, которая будет возвращает true, если переданная строка является полиндромом, и false, если не является
+*/
+
+function palindrome(str) {
+    const notSpace = str.replace(/\s/gim, '').toLowerCase();
+    return (notSpace === notSpace.split('').reverse().join(''));
+}
+
+console.log(palindrome('eye')); // true
+console.log(palindrome('hello')); // false
+console.log(palindrome('А роза упала на лапу Азора')); // true
