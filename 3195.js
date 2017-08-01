6fdf0eefbfbfb6e1d1e2d2376743688b475558f8
gooddima96 <gooddima96@yandex.ru>
2017-08-01 11:36:46 +0300

    Changed names of constants, delete brackets

--- 3195.js
+++ 3195.js
@@ -6,16 +6,16 @@
 */
 
 function randomNumber() {
-    const startTime = Date.now();
-    const min = 1;
-    const max = 2;
-    let myRandom = (min, max) => (min + (max - min) * Math.random());
+    const STARTTIME = Date.now();
+    const MIN = 1;
+    const MAX = 2;
+    let myRandom = (min, max) => min + (max - min) * Math.random();
     
-    const randomMs = myRandom(min, max) * 1000 ^ 0; //Integer milliseconds
+    const randomMs = myRandom(MIN, MAX) * 1000 ^ 0; //Integer milliseconds
     // console.log('Wait:', randomMs);
-    while (randomMs > Date.now() - startTime);
+    while (randomMs > Date.now() - STARTTIME){}
 
-    return myRandom(min, max);
+    return myRandom(MIN, MAX);
 }
 
 console.log(randomNumber());
