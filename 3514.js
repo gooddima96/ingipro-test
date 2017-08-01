3d9a65598af63f5ffea4f2a372802f8c6c1c41b3
gooddima96 <gooddima96@yandex.ru>
2017-08-01 12:45:58 +0300

    Changed let to const, return one number

--- 3514.js
+++ 3514.js
@@ -9,8 +9,7 @@ const array1 = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]; // 5
 const array2 = [1,1,2,-2,5,2,4,4,-1,-2,5]; // -1
 
 function findUneven(array) {
-    let result = [];
-    let obj = {};
+    const obj = {};
 
     for (let i in array) {
         obj[array[i]] = (isNaN(obj[array[i]])) ? 1 : obj[array[i]] + 1;
@@ -18,10 +17,9 @@ function findUneven(array) {
     for (let key in obj) {
         //console.log(key, ':', obj[key]);
         if (obj[key] % 2) {
-            result.push(Number(key));
+            return Number(key);
         }
     }
-    return (result.length <= 1) ? result[0] : result;
 }
 
 console.log(findUneven(array1));
