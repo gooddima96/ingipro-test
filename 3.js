c54e8ef9d0681d538daba47b41ef05a8c7d92015
gooddima96 <gooddima96@yandex.ru>
2017-08-01 10:43:20 +0300

    Changed let to const, fix 0, -1...

--- 3.js
+++ 3.js
@@ -5,8 +5,10 @@
 'use strict';
 
 function chunkArrayInGroups(arr, size) {
-    const result = new Array(arr.length / size ^ 0); //Integer
-    for (let i = 0; i < arr.length / size ^ 0; i++) {
+    const newSize = (size > 0) ? Math.ceil(arr.length / size) : 0;
+    const result = new Array(newSize);
+
+    for (let i = 0; i < newSize; i++) {
         result[i] = new Array(size);
         for (let j = 0; j < size; j++) {
             result[i][j] = arr[i * size + j];
@@ -16,6 +18,5 @@ function chunkArrayInGroups(arr, size) {
 }
 
 console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2)); // [["a", "b"], ["c", "d"]]
-//работает же...
 console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 3)); // [["a", "b", "c"], ["d", undefined, undefined]]
-//console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2)); // [["a", "b", "c"], ["d"]] Могу еще так запилить. Как правильно?
+console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 0)); // []
